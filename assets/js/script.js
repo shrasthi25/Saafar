1c55611a34225a33a15f5c4a81923156cf6c9f0e
shrasthi25 <119516349+shrasthi25@users.noreply.github.com>
2022-11-30 21:30:00 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+//Nav bar
+let formBtn = document.querySelector('#login-btn');
+formBtn.addEventListener('click', () =>{
+    window.location.href="login.html"
+});
+
+//Home page Swiper
+let videoBtn = document.querySelectorAll('.vid-btn');
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+//Review Swiper
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
